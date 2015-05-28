9acc95f655a0179ef849e68329890608c64b3f6b
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-05-28 15:54:40 -0400

    Initial Commit

---
+++ FizzBuzz/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FizzBuzz
+//
+//  Created by Arsalan Vakili on 2015-05-28.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
